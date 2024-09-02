afbd8fd3d4649bf04a3d22fe4f63be802f840206
Coada Emilian <emilian.coada@gmail.com>
2024-09-02 16:46:12 +0200

    feat: created NavBarV2

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import EditProfileComponent from './components/EditProfileForm';
 import HeadbandComponent from './components/Headband';
 import MobileFooterMenuV1 from './components/MobileFooterMenuV1';
 import MobileFooterMenuV2 from './components/MobileFooterMenuV2';
-import NavBar from './components/NavBarV1';
-
+import NavBarV1 from './components/NavBarV1';
+import NavBarV2 from './components/NavBarV2';
 function App() {
   return (
     <>
-      <NavBar />
+      <NavBarV2 />
+      <NavBarV1 />
       <HeadbandComponent />
       <EditProfileComponent />
       <MobileFooterMenuV1 />
