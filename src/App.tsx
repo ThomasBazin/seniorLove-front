b26dd41e6d61d145366526cca63c5f7871aa9284
Coada Emilian <emilian.coada@gmail.com>
2024-09-02 16:28:18 +0200

    feat: created NavBarV1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 //Importing components
 import EditProfileComponent from './components/EditProfileForm';
 import HeadbandComponent from './components/Headband';
 import MobileFooterMenuV1 from './components/MobileFooterMenuV1';
 import MobileFooterMenuV2 from './components/MobileFooterMenuV2';
+import NavBar from './components/NavBarV1';
 
 function App() {
   return (
     <>
+      <NavBar />
       <HeadbandComponent />
       <EditProfileComponent />
       <MobileFooterMenuV1 />
---
+++ src/components/NavBarV1.tsx
@@ -0,0 +1,35 @@
+//NavBar V1 (before connexion)
+
+import Logo from '../assets/images/logo-seniorlove-fondclair.png';
+
+function NavBar() {
+  return (
+    <div className="bg-white p-6 flex lg:flex-row lg:justify-between items-center rounded-lg w-full mx-auto font-bold text-primaryText">
+      <div className="logo_container mb-2 lg:mb-0 flex justify-center w-full lg:w-auto">
+        <a href="#">
+          <img
+            src={Logo}
+            alt="seniorlove logo"
+            className="w-64 h-32 object-contain"
+          />
+        </a>
+      </div>
+      <div className="buttons_container flex flex-col lg:flex-row lg:space-x-4">
+        <a
+          href="#"
+          className="bg-primaryGrey hover:bg-secondaryPink text-secondaryPink hover:text-primaryGrey font-bold py-4 px-6 hidden lg:block rounded-3xl text-sm sm:text-base md:text-lg"
+        >
+          Événements
+        </a>
+        <a
+          href="#"
+          className="bg-primaryGrey hover:bg-secondaryPink text-secondaryPink hover:text-primaryGrey font-bold py-4 px-6 hidden lg:block rounded-3xl text-sm sm:text-base md:text-lg"
+        >
+          Se connecter
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
