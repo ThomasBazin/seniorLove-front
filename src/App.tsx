6ba77d35b638aa5cb3bc680c215672ef4ed50174
ThomasBazin <tom.bazin@gmail.com>
2024-09-02 16:06:45 +0200

    Event component OK

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import ProfileSticker from './components/ProfileSticker/ProfileSticker';
+import EventSticker from './components/EventSticker/EventSticker';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <ProfileSticker />
-      <ProfileSticker />
+      <EventSticker />
     </>
   );
 }
--- src/components/EventSticker/EventSticker.tsx
+++ src/components/EventSticker/EventSticker.tsx
@@ -1,22 +1,17 @@
-import messageIcon from '../../assets/messages.png';
-
 function EventSticker() {
   return (
     <div className="w-64 h-72 bg-white rounded-xl mx-auto shadow-lg relative">
+      <div className="m-2 w-fit absolute top-0 left-1 text-white drop-shadow-2xl text-2xl font-medium">
+        Paris
+      </div>
       <img
-        src="/src/assets/grandma-506341_1280.jpg"
+        src="/src/assets/cours-cuisine.jpg"
         alt="Utilisateur"
         className="h-full object-cover rounded-xl"
       />
       <div className="m-2 w-fit absolute bottom-0 left-1 text-white drop-shadow-2xl text-2xl font-medium">
-        Hélèna, 68
+        Cours de cuisine
       </div>
-      <button
-        type="button"
-        className="p-1 shadow-lg absolute -bottom-4 right-3 bg-white rounded-full overflow-visible"
-      >
-        <img src={messageIcon} alt="message" className="size-10" />
-      </button>
     </div>
   );
 }
