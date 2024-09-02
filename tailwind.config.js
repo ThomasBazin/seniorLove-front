1a0ca159a968d80304804106409f8a4f4167076b
ThomasBazin <tom.bazin@gmail.com>
2024-09-02 15:53:53 +0200

    first draft of sticker component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-    "./src/**/**/*.{html,js,ts,jsx,tsx}",
-    "./docs/**/*.html/"
+    './src/**/*.{html,js,ts,jsx,tsx}',
+    './src/**/**/*.{html,js,ts,jsx,tsx}',
+    './docs/**/*.html/',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryPink: '#F3D6E0',
+        primaryGrey: '#EBEBEA',
+        primaryText: '#59616B',
+        secondaryPink: '#D85071',
+        buttonGreen: '#ADD5AF',
+      },
+    },
   },
   plugins: [],
-}
-
+};
