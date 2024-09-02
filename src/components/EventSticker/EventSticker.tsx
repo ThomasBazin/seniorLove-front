55bea540b82255dd36ebaf6737708330143fa33f
ThomasBazin <tom.bazin@gmail.com>
2024-09-02 16:13:12 +0200

    added shadow to text

--- src/components/EventSticker/EventSticker.tsx
+++ src/components/EventSticker/EventSticker.tsx
@@ -1,7 +1,7 @@
 function EventSticker() {
   return (
     <div className="w-64 h-72 bg-white rounded-xl mx-auto shadow-lg relative">
-      <div className="m-2 w-fit absolute top-0 left-1 text-white drop-shadow-2xl text-2xl font-medium">
+      <div className="m-2 w-fit absolute top-0 left-1 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-2xl font-medium">
         Paris
       </div>
       <img
@@ -9,7 +9,7 @@ function EventSticker() {
         alt="Utilisateur"
         className="h-full object-cover rounded-xl"
       />
-      <div className="m-2 w-fit absolute bottom-0 left-1 text-white drop-shadow-2xl text-2xl font-medium">
+      <div className="m-2 w-fit absolute bottom-0 left-1 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-2xl font-medium">
         Cours de cuisine
       </div>
     </div>
