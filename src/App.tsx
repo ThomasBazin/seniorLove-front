b8d07f9ce52197ab74254c6749cf024120d8368f
Coada Emilian <emilian.coada@gmail.com>
2024-09-04 15:31:56 +0200

    reverted to original app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EventsPage from './pages/EventsPage';
-import EventView from './components/EventView/EventView';
 
 function App() {
   return (
-    <EventView />
-    // <Routes>
-    //   <Route path="/" element={<HomePage />} />
-    //   <Route path="/events" element={<EventsPage />} />
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/events" element={<EventsPage />} />
+    </Routes>
   );
 }
 
