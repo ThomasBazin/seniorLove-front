46f4ef5824a45613adb32f8c3354d6b8b82ddaf7
Coada Emilian <emilian.coada@gmail.com>
2024-09-05 21:48:37 +0200

    feat: managed to add events to connected profile page

--- src/components/pageComponents/HomePageLogged/EventSection.tsx
+++ src/components/pageComponents/HomePageLogged/EventSection.tsx
@@ -1,82 +1,23 @@
 import { useEffect, useState } from 'react';
 import EventSticker from '../../standaloneComponents/EventSticker/EventSticker';
 import DefaultBtn from '../../standaloneComponents/Button/DefaultBtn';
+import { IEvent } from '../../../@types/IEvent';
+import axios from '../../../axios';
 
 export default function EventSection() {
-  const events = [
-    {
-      name: 'Cours de cuisine',
-      location: 'Paris',
-      photo: '/img/events/cuisine.webp',
-      alt: 'Cours de cuisine à Paris',
-    },
-    {
-      name: 'Apéro poterie',
-      location: 'Lyon',
-      photo: '/img/events/poterie.webp',
-      alt: 'Apéro poterie à Lyon',
-    },
-    {
-      name: 'Atelier mixologie',
-      location: 'Marseille',
-      photo: '/img/events/mixologie.webp',
-      alt: 'Atelier mixologie à Marseille',
-    },
-    {
-      name: 'Dégustation de vins',
-      location: 'Bordeaux',
-      photo: '/img/events/degustation.webp',
-      alt: 'Dégustation de vins à Bordeaux',
-    },
-    {
-      name: 'Speed dating',
-      location: 'Nice',
-      photo: '/img/events/dating.webp',
-      alt: 'Speed dating à Nice',
-    },
-    {
-      name: 'Atelier jardinage',
-      location: 'Toulouse',
-      photo: '/img/events/jardinage.webp',
-      alt: 'Atelier jardinage à Toulouse',
-    },
-    {
-      name: 'Cours de photographie',
-      location: 'Strasbourg',
-      photo: '/img/events/photographie.webp',
-      alt: 'Cours de photographie à Strasbourg',
-    },
-    {
-      name: 'Soirée jazz',
-      location: 'Nantes',
-      photo: '/img/events/jazz.webp',
-      alt: 'Soirée jazz à Nantes',
-    },
-    {
-      name: 'Club de lecture virtuel',
-      location: 'Lille',
-      photo: '/img/events/lecture.webp',
-      alt: 'Club de lecture virtuel à Lille',
-    },
-    {
-      name: 'Cours de guitare',
-      location: 'Avignon',
-      photo: '/img/events/guitare.webp',
-      alt: 'Cours de guitare à Avignon',
-    },
-    {
-      name: 'Soirée méditation',
-      location: 'Rennes',
-      photo: '/img/events/meditation.webp',
-      alt: 'Soirée méditation à Rennes',
-    },
-    {
-      name: 'Montgolfière',
-      location: 'Saumur',
-      photo: '/img/events/montgolfiere.webp',
-      alt: 'Montgolfière à Saumur',
-    },
-  ];
+  const [events, setEvents] = useState<IEvent[]>([]);
+  useEffect(() => {
+    const fetchAndSaveEvents = async () => {
+      try {
+        const result = await axios.get('/public/events');
+        setEvents(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAndSaveEvents();
+  }, []);
+
   const [numEvents, setnumEvents] = useState(3);
 
   useEffect(() => {
@@ -101,7 +42,7 @@ export default function EventSection() {
     <div className="w-full py-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-center mx-auto w-11/12 pb-8">
         {events.slice(0, numEvents).map((event) => (
-          <EventSticker event={event} key={event.photo} />
+          <EventSticker event={event} key={event.name} />
         ))}
       </div>
       <DefaultBtn btnText="Voir plus d'évenements" />
--- src/components/pageComponents/HomePageLogged/UsersSection.tsx
+++ src/components/pageComponents/HomePageLogged/UsersSection.tsx
@@ -3,26 +3,26 @@ import ProfileSticker from '../../standaloneComponents/ProfileSticker/ProfileSti
 import DefaultBtn from '../../standaloneComponents/Button/DefaultBtn';
 import { getTokenAndDataFromLocalStorage } from '../../../localStorage/localStorage';
 import axios from '../../../axios';
+import { IUsers } from '../../../@types/IUsers';
 
 export default function UsersSection() {
   const response = getTokenAndDataFromLocalStorage();
   const token = response?.token;
-  const [suggestedUsers, setSuggestedUsers] = useState([]);
-
-  const fetchUsers = async (token: string) => {
-    try {
-      const response = await axios.get('/private/users/me/suggestions', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setSuggestedUsers(response.data);
-    } catch (error) {
-      console.error('Error fetching users:', error);
-    }
-  };
+  const [suggestedUsers, setSuggestedUsers] = useState<IUsers[]>([]);
 
   useEffect(() => {
+    const fetchUsers = async (token: string) => {
+      try {
+        const response = await axios.get('/private/users', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setSuggestedUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
     if (token) {
       fetchUsers(token);
     }
@@ -51,7 +51,7 @@ export default function UsersSection() {
     <div className="w-full py-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-center mx-auto w-11/12 pb-8">
         {suggestedUsers.slice(0, numProfiles).map((user) => (
-          <ProfileSticker user={user} key={user.picture} />
+          <ProfileSticker user={user} key={user.name} />
         ))}
       </div>
       <DefaultBtn btnText="Voir plus de profil" />
