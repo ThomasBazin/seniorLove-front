b0ee3b8407dbb0ed3b584b0ad653dfd5a7d77860
LevequeKevin <126432986+LevequeKevin@users.noreply.github.com>
2024-09-06 09:52:09 +0200

    refactor: Update EventView component layout and styling

--- src/components/pageComponents/EventPage/EventView/EventView.tsx
+++ src/components/pageComponents/EventPage/EventView/EventView.tsx
@@ -5,8 +5,8 @@ export default function EventView() {
   const location = useLocation();
   const event = location.state?.event as IEvent; // Retrieve the passed event
   return (
-    <div className="w-full min-h-full flex-grow flex flex-col items-center bg-primaryGrey">
-      <div className="pt-8 px-8 items-center max-w-7xl">
+    <div className="w-full min-h-full flex-grow flex bg-primaryGrey">
+      <div className="pt-8 px-8 max-w-7xl w-full justify-center mx-auto ">
         <div className="h-72 rounded-xl relative mb-4">
           <img
             src={event.picture}
@@ -15,40 +15,41 @@ export default function EventView() {
           />
         </div>
 
-        <div className="">
-          <div className="flex flex-col justify-between">
-            <div className="p-2 w-full text-primaryText text-xl text-center mb-4">
-              <p>{event.name}</p>
-            </div>{' '}
-            <div className="flex flex-row-reverse justify-between">
-              <div className="w-80 flex flex-col pl-20 gap-4">
-                <p className="text-primaryText italic">
-                  <span className="font-semibold">Date</span> : {event.date}
+        <div className="flex flex-col justify-between">
+          <div className="p-2 w-full text-primaryText text-xl text-center mb-4">
+            <p>{event.name}</p>
+          </div>{' '}
+          {/* Aside */}
+          <div className="flex flex-col md:flex-row-reverse md:justify-between">
+            <div className="flex md:flex-col md:pl-20 gap-4 flex-wrap justify-center">
+              <p className="text-primaryText italic">
+                <span className="font-semibold">Date</span> : {event.date}
+              </p>
+              <p className="text-primaryText italic">
+                <span className="font-semibold">Lieu</span> {event.location},
+                France
+              </p>
+              <div>
+                <p className="text-primaryText italic mb-1">
+                  <span className="font-semibold">Centres d&apos;intérêt</span>{' '}
+                  :
                 </p>
-                <p className="text-primaryText italic">
-                  <span className="font-semibold">Lieu</span> {event.location},
-                  France
-                </p>
-                <ul className="text-primaryText italic">
-                  <p className="text-primaryText italic mb-1">
-                    <span className="font-semibold">
-                      Centres d&apos;intérêt
-                    </span>{' '}
-                    :
-                  </p>
+                <div className="text-primaryText">
                   {event.hobbies.map((hobby) => (
-                    <li key={hobby.name}>{hobby.name}</li>
+                    <p key={hobby.name}>{hobby.name}</p>
                   ))}
-                </ul>
-              </div>
-              <div className="w-80 h-fit mb-4 md:w-3/5 ">
-                <p className="text-primaryText w-auto m-4 italic py-2">
-                  {event.description}
-                </p>
+                </div>
               </div>
             </div>
+            {/* Description */}
+            <div className="md:w-4/5 ">
+              <p className="text-primaryText py-6 md:py-0 italic">
+                {event.description}
+              </p>
+            </div>
           </div>
-          {/* <div className="button_container flex justify-center">
+        </div>
+        {/* <div className="button_container flex justify-center">
               <Link
                 to="#"
                 className="bg-buttonGreen text-primaryText text-center w-2/4 font-bold py-2 px-3  rounded-lg shadow-lg sm:text-base md:w-1/4 md:text-lg lg:block lg:text-xs xl:text-lg italic"
@@ -56,7 +57,6 @@ export default function EventView() {
                 Je participe
               </Link>
             </div> */}
-        </div>
       </div>
     </div>
   );
