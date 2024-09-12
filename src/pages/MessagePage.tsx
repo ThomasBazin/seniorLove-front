d52e0d1e101278b95d7153ccf6f2713a70994172
Coada Emilian <emilian.coada@gmail.com>
2024-09-12 21:46:47 +0200

    merge attempt

--- src/pages/MessagePage.tsx
+++ src/pages/MessagePage.tsx
@@ -1,5 +1,4 @@
 import MessagesField from '../components/Messages/MessagesField';
-import UserHeadband from '../components/standaloneComponents/UserHeadband/UserHeadband';
 
 interface MessagesPageProps {
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,9 +7,6 @@ interface MessagesPageProps {
 export default function MessagePage({ setIsAuthenticated }: MessagesPageProps) {
   return (
     <>
-      <div>
-        <UserHeadband setIsAuthenticated={setIsAuthenticated} />
-      </div>
       <main className="w-full min-h-screen flex-grow flex flex-col items-center bg-backgroundPink pb-8">
         <MessagesField />
       </main>
