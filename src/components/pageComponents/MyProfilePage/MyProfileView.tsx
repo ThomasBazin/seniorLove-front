af5f0d9900fafabc55d7f7ef2628cc4d332981dd
ThomasBazin <tom.bazin@gmail.com>
2024-09-12 17:45:03 +0200

    error 500 myprofile page OK

--- src/components/pageComponents/MyProfilePage/MyProfileView.tsx
+++ src/components/pageComponents/MyProfilePage/MyProfileView.tsx
@@ -4,11 +4,16 @@ import axios from '../../../axios';
 import { IUsers } from '../../../@types/IUsers';
 import EventSticker from '../../standaloneComponents/EventSticker/EventSticker';
 import DefaultBtn from '../../standaloneComponents/Button/DefaultBtn'; // Assurez-vous que DefaultBtn est importé correctement
+import Error500Page from '../../../pages/Error500Page';
 
 export default function MyProfileView() {
   const { myId } = useParams<{ myId: string }>();
+
+  // STATE 1 : my profile
   const [me, setMe] = useState<IUsers | null>(null);
-  const [error, setError] = useState<string | null>(null);
+
+  // STATE 2 : error server
+  const [serverError, setServerError] = useState(false);
 
   useEffect(() => {
     const fetchConnectedUser = async () => {
@@ -17,14 +22,14 @@ export default function MyProfileView() {
         setMe(response.data); // Stocke les données de l'utilisateur dans le state
       } catch (e) {
         console.error(e);
-        setError('Error fetching your profile');
+        setServerError(true);
       }
     };
     fetchConnectedUser();
   }, [myId]); // L'ID de l'utilisateur est utilisé comme dépendance pour relancer le fetch si nécessaire
 
-  if (error) {
-    return <p className="text-red-500 text-center">{error}</p>;
+  if (serverError) {
+    return <Error500Page />;
   }
 
   // message de chargement tant que les données ne sont pas disponibles
