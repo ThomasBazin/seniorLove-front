b9ea9bb8f04695e1a75f9f833cbc910536a73dd0
LevequeKevin <126432986+LevequeKevin@users.noreply.github.com>
2024-09-14 19:51:56 +0200

    chores: add comments in profileView

--- src/components/pageComponents/ProfilePage/MainProfilePage.tsx
+++ src/components/pageComponents/ProfilePage/MainProfilePage.tsx
@@ -1,7 +1,5 @@
-import ProfileView from './ProfileView/ProfileView';
 import UsersProfile from './ProfileView/UsersProfile';
 
 export default function MainProfilePage() {
-  // return <ProfileView />;
   return <UsersProfile />;
 }
