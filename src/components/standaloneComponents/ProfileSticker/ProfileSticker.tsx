ee82dac3539a6f2658d63999e249c75a976a63a5
LevequeKevin <126432986+LevequeKevin@users.noreply.github.com>
2024-09-17 13:11:02 +0200

    fix: error typescript & alt attribute profile sticker

--- src/components/standaloneComponents/ProfileSticker/ProfileSticker.tsx
+++ src/components/standaloneComponents/ProfileSticker/ProfileSticker.tsx
@@ -16,7 +16,7 @@ function ProfileSticker({ user }: ProfileStickerProps) {
       <div className="h-72 w-72 2xl:w-80 rounded-xl mx-auto shadow-lg relative">
         <img
           src={user.picture}
-          alt={user.alt}
+          alt={user.name}
           className="object-cover rounded-xl h-72 w-72 2xl:w-80"
         />
         <div className="m-2 w-fit absolute bottom-0 left-1 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] text-2xl font-medium">
