b5a60c8fda21bdc7011ef1a466c9682748966d0e
ThomasBazin <tom.bazin@gmail.com>
2024-09-17 14:21:23 +0200

    unit-tests utils functions ok

--- src/utils/computeAge.test.ts
+++ src/utils/computeAge.test.ts
@@ -15,6 +15,12 @@ describe('Test computeAge', () => {
     }).toThrow('You must provide a string !');
   });
 
+  test('Should throw an error if argument passed is not a valid date', () => {
+    expect(() => computeAge('24-12-03')).toThrow(
+      'You must provide a valid ISO date format YYYY-MM-DD'
+    );
+  });
+
   test('ISO format YYYY-MM-DD 74y.o', () => {
     expect(computeAge('1950-06-15')).toBe(74);
   });
@@ -23,31 +29,11 @@ describe('Test computeAge', () => {
     expect(computeAge('2000-09-16')).toBe(24);
   });
 
-  test('Format YYYY/MM/DD', () => {
-    expect(computeAge('1950/06/15')).toBe(74);
-  });
-
-  test('Format YY/MM/DD', () => {
-    expect(computeAge('50/06/15')).toBe(74);
-  });
-
-  test('Format YY-MM-DD', () => {
-    expect(computeAge('50-06-15')).toBe(74);
-  });
-
   test('Year only', () => {
     expect(computeAge('1950')).toBe(74);
   });
 
   test('End of year birthday ISO format YYYY-MM-DD', () => {
     expect(computeAge('1950-12-31')).toBe(73);
-  });
-
-  test('ISO Date Format with time', () => {
-    expect(computeAge('Thu Jan 01 1950 01:00:01 GMT+0100')).toBe(74);
-  });
-
-  test('Short Date Format MM/DD/YYYY', () => {
-    expect(computeAge('06/14/1964')).toBe(60);
   });
 });
