1f0092d33ddbf692ef11cc4338be2a25d2f56530
LevequeKevin <126432986+LevequeKevin@users.noreply.github.com>
2024-09-18 11:10:34 +0200

    feat: add regex for password validation

--- src/utils/joiValidateFormV4.ts
+++ src/utils/joiValidateFormV4.ts
@@ -14,14 +14,21 @@ const stepFourSchema = Joi.object({
       'string.pattern.base': 'Veuillez entrer une adresse mail valide !',
       'any.required': 'Veuillez entrer une adresse mail !',
     }),
-  password: Joi.string().min(12).max(255).required().messages({
-    'string.base': 'Veuillez entrer une adresse mail valide !',
-    'string.min': 'Le mot de passe doit contenir au moins 12 caractères.',
-    'string.max':
-      'Le mot de passe est trop long, entrez un mot de passe plus court',
-    'string.empty': 'Veuillez indiquer votre mot de passe et le confirmer.',
-    'any.required': 'Veuillez entrer un mot de passe',
-  }),
+  password: Joi.string()
+    .min(12)
+    .max(36)
+    .pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,36}$/)
+    .required()
+    .messages({
+      'string.base': 'Veuillez entrer une adresse mail valide !',
+      'string.min': 'Le mot de passe doit contenir au moins 12 caractères.',
+      'string.max':
+        'Le mot de passe est trop long, entrez un mot de passe plus court.',
+      'string.empty': 'Veuillez indiquer votre mot de passe et le confirmer.',
+      'any.required': 'Veuillez entrer un mot de passe.',
+      'string.pattern.base':
+        'Le mot de passe doit contenir au moins une majuscule, un chiffre et un caractère spécial.',
+    }),
   repeatPassword: Joi.any().valid(Joi.ref('password')).required().messages({
     'any.only': 'Le mot de passe et sa confirmation doivent être identiques.',
     'any.required': 'Veuillez confirmer votre mot de passe.',
