cd0b022c28747c58aa4ba5b0ac28a1f53632a08e
ThomasBazin <tom.bazin@gmail.com>
2024-11-24 22:24:33 +0100

    refacto main navbar and mobile in same component

--- src/components/standaloneComponents/NavBar/NavBar.tsx
+++ src/components/standaloneComponents/NavBar/NavBar.tsx
@@ -1,38 +1,61 @@
 // NavBar V1 (before connexion)
 
 import { Link } from 'react-router-dom';
-import MobileFooterMenuV1 from '../MobileNavBar/MobileNavBar';
-import Logo from '/img/logo-text-seniorlove.webp';
+import textLogo from '/img/logo-text-seniorlove.webp';
+import logo from '/icon/logo-pink-background.png';
+import connexionLogo from '/icon/connexion.png';
+import eventLogo from '/icon/evenement.png';
 
 export default function NavBarV1() {
-  const NavBarLinks = [
-    { text: 'Evènements', to: '/events' },
-    { text: 'Se connecter', to: '/login' },
+  const navItems = [
+    {
+      img: eventLogo,
+      text: 'Événements',
+      href: '/events',
+      mobileOnly: false,
+    },
+    {
+      img: logo,
+      text: 'Accueil',
+      href: '/',
+      mobileOnly: true,
+    },
+    {
+      img: connexionLogo,
+      text: 'Connexion',
+      href: '/login',
+      mobileOnly: false,
+    },
   ];
 
   return (
-    <header className="bg-white bg-opacity-90 md:sticky top-0 w-full py-4 z-10">
+    <header className="bg-white bg-opacity-90 md:sticky top-0 w-full py-4">
       <nav className="flex justify-center md:justify-between items-center w-full px-3">
         <Link to="/">
           <img
-            src={Logo}
+            src={textLogo}
             alt="Retour à l'accueil"
             className="max-w-36 lg:max-w-52"
           />
         </Link>
-        <div className="flex gap-6">
-          {NavBarLinks.map((link) => (
+        <div className="flex fixed px-3 pt-2 md:gap-6 bottom-0 md:top-3 md:right-0 bg-white md:bg-inherit shadow-mobileNav md:shadow-none w-full md:w-fit justify-around items-center md:items-start ">
+          {navItems.map((item) => (
             <Link
-              to={link.to}
-              key={link.text}
-              className="text-secondaryPink hover:text-primaryText font-bold py-2 px-3 hidden md:block"
+              to={item.href}
+              key={item.text}
+              className={`text-primaryText md:text-secondaryPink md:hover:text-primaryText md:font-bold text-sm md:text-base mb-1 md:mb-0 md:px-3 ${item.mobileOnly && 'md:hidden'}`}
             >
-              {link.text}
+              <img
+                src={item.img}
+                alt=""
+                aria-hidden="true"
+                className="mx-auto w-8 object-contain md:hidden"
+              />
+              {item.text}
             </Link>
           ))}
         </div>
       </nav>
-      <MobileFooterMenuV1 />
     </header>
   );
 }
